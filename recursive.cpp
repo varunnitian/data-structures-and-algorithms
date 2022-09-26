40a04e30efeb1dcce8fa83ca7ba1ba224e1e60c2
varun jain <98322627+varunnitian@users.noreply.github.com>
2022-09-27 00:15:23 +0530

    Add files via upload

---
+++ recursive.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int fun(int n){
+    if(n>0){
+        return fun(n-1)+n;
+    }
+    return 0;
+}
+int main(){
+    int a=5;
+    cout<<fun(a);
+    return 0;
+}
