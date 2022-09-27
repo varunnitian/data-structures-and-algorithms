79714e17010d75cd81e27b1b682597a913b2073c
varun jain <98322627+varunnitian@users.noreply.github.com>
2022-09-27 13:51:06 +0530

    Add files via upload

---
+++ tail_recursion.cpp
@@ -0,0 +1,35 @@
+
+//recursion-to-loop//
+// #include<iostream>
+// using namespace std;
+// void fun(int n)
+// {
+// while(n>0)
+// {
+//     cout<<n<<" ";
+//     n--;
+// }
+// }
+// int main()
+// {
+//     int a=3;
+//     fun(a);
+//     return 0;
+// }
+//loop-to-recursion//
+// #include<iostream>
+// using namespace std;
+// void fun(int n)
+// {
+// if(n>0)
+// {
+//     cout<<n<<" ";
+//     fun(n-1);
+// }
+// }
+// int main()
+// {
+//     int a=3;
+//     fun(a);
+//     return 0;
+// }
